e283ebb76023059701b778dd8192ab608344dc48
Mohamud Olow <khalif18@live.com>
2019-10-21 09:25:08 -0700

    draw the y axis of the graph

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
 var heatmapGraph = document.getElementById('heatmapGraph');
 var width = 0.9 * heatmapGraph.parentElement.clientWidth;
 var height = 0.86 * heatmapGraph.parentElement.clientHeight;
-console.log(height);
+var padding = {"top": 10, "right": 10, "bottom": 30, "left": 80};
 
 var svg = d3.select('#heatmapGraph')
 .append('svg')
@@ -14,6 +14,8 @@ window.onresize = () => location.reload();
 
 const drawGraph = (data) => {
     
+    //yAxisScale.domain(data, d => d.variance);
+    
     //define x-axis scale
     var xAxisScale = d3.scaleLinear()
     .domain([d3.min(data, d => d.year), d3.max(data, d => d.year)])
@@ -24,8 +26,22 @@ const drawGraph = (data) => {
     
     //append the x axis to svg
     svg.append("g")
-    .attr('transform', 'translate(20, '+ (height-20) + ')')
+    .attr('transform', 'translate('+ padding.left +', '+ (height-padding.bottom) + ')')
     .call(xAxis);
+    
+    //define y-axis scale
+    var yAxisScale = d3.scaleBand()
+    .domain(data.map(d => d.month))
+    .rangeRound([height-padding.top-padding.bottom, 0]);
+    var yAxis = d3.axisLeft(yAxisScale).tickValues(yAxisScale.domain()).tickFormat(month => {
+        var date = new Date(0);
+        date.setUTCMonth(month);
+        return d3.timeFormat('%B')(date);
+    });
+    
+    svg.append('g')
+    .attr('transform', 'translate('+ padding.left +', '+ padding.right + ')')
+    .call(yAxis);
 }
 
 d3.json(url).then(data => {
